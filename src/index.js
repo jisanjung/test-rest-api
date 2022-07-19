292f856d1a0406df720747c5d4281debccc1fe86
Jisan Jung <jjjung13@gmail.com>
2022-07-18 21:32:52 -0400

    create route and middleware

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 const express = require("express");
-const app = express();
+const serverless = require("serverless-http");
 
+const app = express();
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.json({
+        message: "jason jung"
+    });
+});
+
+app.use("/.netlify/functions/api", router);
+
+module.exports.handler = serverless(app);
